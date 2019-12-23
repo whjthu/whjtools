90a88b34a1b8f002eb1277f2526145d37d97aa1a
whjthu <haojie0429@gmail.com>
2019-12-23 21:58:21 +0800

    Tool to get AWS a group of ec2 ips fast

---
+++ awsip/README.md
@@ -0,0 +1,4 @@
+# Get IP from AWS console fast
+1. Select all ec2s you want to get the ips from (at least 2)
+2. Copy the descriptions to file `details'
+3. Run getIPs.py, then the result will be output into file `ips'
---
+++ awsip/getIPs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+details = []
+ips = []
+with open('details', 'r') as fin:
+    for line in fin:
+        details.append(line)
+
+for item in details:
+    ip = '.'.join(item.split(': ')[1].split('.')[0].split('-')[1:])
+    ips.append(ip)
+
+with open('ips', 'w') as fout:
+    for ip in ips:
+        fout.write(ip+'\n')
